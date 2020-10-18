bcea0e8fe459c5ca056bd730f38089c2a371bff6
BevoLEt <kathor888@gmail.com>
2020-10-18 05:06:39 +0000

    webview porting

---
+++ HealthWindow/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { BackHandler} from 'react-native';
+import { WebView } from 'react-native-webview';
+
+// ...
+class App extends Component {
+  webView = {
+    canGoBack: false,
+    ref: null,
+  }
+
+  onAndroidBackPress = () => {
+    if (this.webView.canGoBack && this.webView.ref) {
+      this.webView.ref.goBack();
+      return true;
+    }
+    return false;
+  }
+
+  componentDidMount() {
+    if (Platform.OS === 'android') {
+      BackHandler.addEventListener('hardwareBackPress', this.onAndroidBackPress);
+    }
+  }
+
+  componentWillUnmount() {
+    if (Platform.OS === 'android') {
+      BackHandler.removeEventListener('hardwareBackPress');
+    }
+  }
+
+  render() {
+    return (
+      <WebView
+        source={{ uri: "https://www.naver.com" }}
+        ref={(webView) => { this.webView.ref = webView; }}
+        onNavigationStateChange={(navState) => { this.webView.canGoBack = navState.canGoBack; }}
+      />
+    );
+  }
+}
+
+export default App
+
+
+// import React, { Component } from 'react';
+// import { WebView } from 'react-native-webview';
+
+// // ...
+// class App extends Component {
+//   render() {
+//     return (
+//       <WebView 
+// 		  source={{ uri: 'https://www.naver.com' }} 
+// 		  />
+//     );
+//   }
+// }
+
+// export default App
